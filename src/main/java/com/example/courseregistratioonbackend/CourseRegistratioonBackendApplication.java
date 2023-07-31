c874fcae24536a9b43724c54641ec547dee2e623
thing-zoo <cathy4025@gmail.com>
2023-07-31 21:16:05 +0900

    feat: init commit

---
+++ src/main/java/com/example/courseregistratioonbackend/CourseRegistratioonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseregistratioonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistratioonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRegistratioonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courseregistratioonbackend/CourseRegistratioonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseregistratioonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistratioonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
