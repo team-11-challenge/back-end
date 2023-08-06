bb7ad55d976b833f21475e5ed47431626aedabca
Mi <mi_aa_@naver.com>
2023-08-06 15:10:22 +0900

    feat: Swagger 초기 설정 및 적용
    SwaggerConfig: Swagger 기본 설정
    BascketController, CourseController, RegistrationController, StudentController: Swagger 기본 tag 추가

--- src/main/java/com/example/courseregistratioonbackend/domain/registration/controller/RegistrationController.java
+++ src/main/java/com/example/courseregistratioonbackend/domain/registration/controller/RegistrationController.java
@@ -1,4 +1,7 @@
 package com.example.courseregistratioonbackend.domain.registration.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "수강신청 관련 API", description = "수강신청 관련 API")
 public class RegistrationController {
 }
