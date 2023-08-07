bac5ac68294c1779ddb203fa2a1689819239b4d3
thing-zoo <cathy4025@gmail.com>
2023-08-07 20:44:39 +0900

    feat: JWT 토큰 적용

--- src/main/java/com/example/courseregistratioonbackend/domain/registration/controller/RegistrationController.java
+++ src/main/java/com/example/courseregistratioonbackend/domain/registration/controller/RegistrationController.java
@@ -2,9 +2,11 @@ package com.example.courseregistratioonbackend.domain.registration.controller;
 
 import com.example.courseregistratioonbackend.domain.registration.service.RegistrationService;
 import com.example.courseregistratioonbackend.global.responsedto.ApiResponse;
+import com.example.courseregistratioonbackend.global.security.userdetails.UserDetailsImpl;
 import com.example.courseregistratioonbackend.global.utils.ResponseUtils;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "수강신청 관련 API", description = "수강신청 관련 API")
@@ -14,22 +16,21 @@ import org.springframework.web.bind.annotation.*;
 public class RegistrationController {
     private final RegistrationService registrationService;
 
-    // TODO: @AuthenticationPrincipal UserDetailsImpl userDetails 추가
     @PostMapping("/{courseId}")
-    public ApiResponse<?> register(@PathVariable Long courseId) {
-        return ResponseUtils.ok(registrationService.register(courseId, 1L));
+    public ApiResponse<?> register(@PathVariable Long courseId,
+                                   @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseUtils.ok(registrationService.register(courseId, userDetails.getStudentUser().getId()));
     }
 
-    // TODO: @AuthenticationPrincipal UserDetailsImpl userDetails 추가
     @DeleteMapping("/{registrationId}")
-    public ApiResponse<?> cancel(@PathVariable Long registrationId) {
-        return ResponseUtils.ok(registrationService.cancel(registrationId, 1L));
+    public ApiResponse<?> cancel(@PathVariable Long registrationId,
+                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseUtils.ok(registrationService.cancel(registrationId, userDetails.getStudentUser().getId()));
     }
 
-    // TODO: @AuthenticationPrincipal UserDetailsImpl userDetails 추가
     @GetMapping
-    public ApiResponse<?> getRegistration() {
-        return ResponseUtils.ok(registrationService.getRegistration(1L));
+    public ApiResponse<?> getRegistration(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseUtils.ok(registrationService.getRegistration(userDetails.getStudentUser().getId()));
     }
 }
 
