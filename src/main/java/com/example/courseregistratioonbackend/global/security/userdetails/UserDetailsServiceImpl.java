c49decf3c8b7d25aa001c1e4dca3597347ef4a6c
Mi <mi_aa_@naver.com>
2023-08-12 14:53:30 +0900

    chore: UserNotFoundException package 이동

--- src/main/java/com/example/courseregistratioonbackend/global/security/userdetails/UserDetailsServiceImpl.java
+++ src/main/java/com/example/courseregistratioonbackend/global/security/userdetails/UserDetailsServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.courseregistratioonbackend.global.security.userdetails;
 
 import com.example.courseregistratioonbackend.domain.student.entity.Student;
-import com.example.courseregistratioonbackend.domain.student.exception.UserNotFoundException;
 import com.example.courseregistratioonbackend.domain.student.repository.StudentRepository;
 import com.example.courseregistratioonbackend.global.enums.ErrorCode;
+import com.example.courseregistratioonbackend.global.security.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
