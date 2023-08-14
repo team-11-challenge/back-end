73601539a4d580b096101d47a0664f1b490cccc4
YiHwanRyu <yihwandev@gmail.com>
2023-08-14 14:31:17 +0900

    chore: 에러 코드 추가(JWT)

--- src/main/java/com/example/courseregistratioonbackend/global/enums/ErrorCode.java
+++ src/main/java/com/example/courseregistratioonbackend/global/enums/ErrorCode.java
@@ -11,9 +11,11 @@ public enum ErrorCode {
     COLLEGE_NAME_IS_REQUIRED(BAD_REQUEST, "대학 입력은 필수 입니다."),
     USER_LOGIN_FAILURE(BAD_REQUEST, "로그인 실패하였습니다."),
     CREDIT_EXCEEDED(BAD_REQUEST, "이수 가능 학점이 초과되었습니다."),
+    JWT_PREFIX_EXPIRATION(BAD_REQUEST, "인증 식별자 형식이 잘못되었습니다."),
 
     /* 401 UNAUTHORIZED : 인증되지 않았다는 의미입니다. */
     UNAUTHORIZED_USER(UNAUTHORIZED, "인증에 실패하였습니다."),
+    JWT_EXPIRATION(UNAUTHORIZED, "유효시간 만료, 재인증이 필요합니다."),
 
     /* 403 FORBIDDEN : 클라이언트가 콘텐츠에 접근할 권리를 가지고 있지 않다는 의미입니다.*/
     NO_AUTHORITY_TO_REGISTRATION(FORBIDDEN, "해당 수강신청을 한 사용자가 아닙니다."),
