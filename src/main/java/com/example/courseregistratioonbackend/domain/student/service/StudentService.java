403a30024898e4b05a7deecdbe28397efaec15df
Mi <mi_aa_@naver.com>
2023-08-24 16:38:37 +0900

    refactor: 시간표 조회 쿼리 최적화

--- src/main/java/com/example/courseregistratioonbackend/domain/student/service/StudentService.java
+++ src/main/java/com/example/courseregistratioonbackend/domain/student/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.example.courseregistratioonbackend.domain.student.service;
 
+import com.example.courseregistratioonbackend.domain.course.entity.QCourse;
+import com.example.courseregistratioonbackend.domain.registration.entity.QRegistration;
 import com.example.courseregistratioonbackend.domain.registration.entity.Registration;
 import com.example.courseregistratioonbackend.domain.registration.repository.RegistrationRepository;
 import com.example.courseregistratioonbackend.domain.student.dto.StudentInfoDto;
@@ -7,6 +9,8 @@ import com.example.courseregistratioonbackend.domain.student.dto.TimetableRespon
 import com.example.courseregistratioonbackend.domain.student.entity.Student;
 import com.example.courseregistratioonbackend.domain.student.execption.StudentNotFoundException;
 import com.example.courseregistratioonbackend.domain.student.repository.StudentRepository;
+import com.example.courseregistratioonbackend.global.parsing.entity.QSubject;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +25,7 @@ import static com.example.courseregistratioonbackend.global.enums.ErrorCode.STUD
 public class StudentService {
     private final StudentRepository studentRepository;
     private final RegistrationRepository registrationRepository;
+    private final JPAQueryFactory queryFactory;
 
     // 시간표 조회
     public List<TimetableResponseDto> getTimetable(Long studentId) {
@@ -28,8 +33,18 @@ public class StudentService {
                 () -> new StudentNotFoundException(STUDENT_NOT_FOUND)
         );
 
-        List<Registration> registrationList = registrationRepository.findByStudent(student);
-        return registrationList.stream()
+        QRegistration qRegistration = QRegistration.registration;
+        QCourse qCourse = QCourse.course;
+        QSubject qSubject = QSubject.subject;
+        List<Registration> registrationList = queryFactory
+                .select(qRegistration)
+                .from(qRegistration)
+                .join(qRegistration.course, qCourse).fetchJoin()
+                .join(qCourse.subject, qSubject).fetchJoin()
+                .where(qRegistration.student.eq(student))
+                .fetch();
+
+        List<TimetableResponseDto> timetableResponseDtoList =  registrationList.stream()
                 .map(registration -> {
                     String courseNM = registration.getCourse().getSubject().getSubjectNM();
                     String[] timetableStrList = registration.getCourse().getTimetable().split(",");
@@ -40,6 +55,7 @@ public class StudentService {
                     return new TimetableResponseDto(courseNM, timetable);
                 })
                 .collect(Collectors.toList());
+        return timetableResponseDtoList;
     }
 
 
