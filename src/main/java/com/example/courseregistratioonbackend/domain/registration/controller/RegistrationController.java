81240959167095433384b6317f9d1e5dc37015fd
YiHwanRyu <yihwandev@gmail.com>
2023-08-30 09:41:07 +0900

    chore: 테스트 후 스핀락 제거, 비관락 재적용

--- src/main/java/com/example/courseregistratioonbackend/domain/registration/controller/RegistrationController.java
+++ src/main/java/com/example/courseregistratioonbackend/domain/registration/controller/RegistrationController.java
@@ -1,14 +1,20 @@
 package com.example.courseregistratioonbackend.domain.registration.controller;
 
-import com.example.courseregistratioonbackend.domain.registration.facade.RedisLockRegistrationFacade;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.courseregistratioonbackend.domain.registration.service.RegistrationService;
 import com.example.courseregistratioonbackend.global.responsedto.ApiResponse;
 import com.example.courseregistratioonbackend.global.security.userdetails.UserDetailsImpl;
 import com.example.courseregistratioonbackend.global.utils.ResponseUtils;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "수강신청 관련 API", description = "수강신청 관련 API")
 @RestController
@@ -16,12 +22,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/registration")
 public class RegistrationController {
     private final RegistrationService registrationService;
-    private final RedisLockRegistrationFacade redisLockRegistrationFacade;
 
     @PostMapping("/{courseId}")
     public ApiResponse<?> register(@PathVariable Long courseId,
-                                   @AuthenticationPrincipal UserDetailsImpl userDetails) throws InterruptedException {
-        return ResponseUtils.ok(redisLockRegistrationFacade.registerByRedisLock(courseId, userDetails.getStudentUser().getId()));
+                                   @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseUtils.ok(registrationService.register(courseId, userDetails.getStudentUser().getId()));
     }
 
     @DeleteMapping("/{registrationId}")
