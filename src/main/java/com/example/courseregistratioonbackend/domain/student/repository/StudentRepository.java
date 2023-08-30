6c1297f0257d949b77b79e3bc4094b43b9012499
Mi <mi_aa_@naver.com>
2023-08-30 20:16:23 +0900

    feat: Redis Cache 구현

--- src/main/java/com/example/courseregistratioonbackend/domain/student/repository/StudentRepository.java
+++ src/main/java/com/example/courseregistratioonbackend/domain/student/repository/StudentRepository.java
@@ -1,14 +1,14 @@
 package com.example.courseregistratioonbackend.domain.student.repository;
 
 import com.example.courseregistratioonbackend.domain.student.entity.Student;
+
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
-
-import jakarta.persistence.LockModeType;
-import org.springframework.data.repository.query.Param;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 	Optional<Student> findByStudentNum(String studentNum);
